ee098583792c2e9c55ea2ecd35757b7ba707497e
riy8n <mahafujhossainriyan02@gmail.com>
2025-07-19 01:24:13 +0600

    Added the axiossecure

--- src/Utilities/Axios/UseAxios.jsx
+++ src/Utilities/Axios/UseAxios.jsx
@@ -1,11 +1,19 @@
-import axios from "axios";
-
-const axiosInstance = axios.create({
-    baseURL: `http://localhost:5000`
-})
+import axios from 'axios';
 
 const useAxios = () => {
-    return axiosInstance;
+  const instance = axios.create({
+    baseURL: 'http://localhost:5000', // or your API endpoint
+  });
+
+  instance.interceptors.request.use(config => {
+    const token = localStorage.getItem('access-token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  });
+
+  return instance;
 };
 
 export default useAxios;
